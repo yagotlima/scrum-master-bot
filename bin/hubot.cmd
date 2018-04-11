f460a40f1e60c381d8efc91c7f600580ccce32a3
Yago Lima <yago@192.168.2.103>
2018-04-10 22:06:05 -0300

    [UPDATE] Projeto renomeado em todos os arquivos

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "scrum-master-yoda" %* 
+node_modules\.bin\hubot.cmd --name "scrum-master" %* 
